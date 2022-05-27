53a82bd0fa1430a6a48ed3a28c4cf373bdea3f86
HITARTH KOTECHA <102938817+HRK1503@users.noreply.github.com>
2022-05-27 13:47:38 +0530

    pod:week1-day4.cpp

---
+++ pod:week1-day4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num;
+	char st[50];
+	cin >> num >> st;    //Reading input from STDIN
+	cout << num*2<< endl<<st;	// Writing output to STDOUT
+}
