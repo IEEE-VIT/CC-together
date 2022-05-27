baed3ff672fcb10d91705729cdc34edc689c9b31
HITARTH KOTECHA <102938817+HRK1503@users.noreply.github.com>
2022-05-27 13:54:57 +0530

    Delete pod:week1-day4.cpp

--- pod:week1-day4.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-	int num;
-	char st[50];
-	cin >> num >> st;    //Reading input from STDIN
-	cout << num*2<< endl<<st;	// Writing output to STDOUT
-}
