5205ca2e29008ced673c9949d9876d141dd1859d
Sai Sahith <91070868+sai-sahith7@users.noreply.github.com>
2022-05-31 20:35:53 +0530

    commit day-1
    upated comments

--- POD/Week2-Strings/day1.py
+++ POD/Week2-Strings/day1.py
@@ -1,15 +1,15 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        d = dict()
+        d = dict()  #dictionary to allot characters of s as keys and characters of t as values
         flag = True
         l = len(t)
         for i in range(l):
-            if s[i] not in d.keys():
-                if t[i] in d.values():
+            if s[i] not in d.keys():   #checking if the character of s has already been allotted to a character of t
+                if t[i] in d.values(): #checking if character of t has been allotted to different character of s
                     flag = False
                     break
-                d[s[i]] = t[i]
-            elif d[s[i]] != t[i]:
+                d[s[i]] = t[i]     #alloting a character of s to character of t
+            elif d[s[i]] != t[i]:  #checking if character of t is not equal to previously allotted character in dictionary
                 flag = False
                 break
         return flag
